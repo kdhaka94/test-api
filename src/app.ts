fc4d8bc388d4312daa58aa7fe1b483e2cbea7958
Kuldeep Dhaka <kuldeep.dhaka@techinfinito.com>
2023-05-16 14:24:15 +0530

    first commit

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,10 @@ import MessageResponse from './interfaces/MessageResponse';
 import mongoose from 'mongoose';
 
 require('dotenv').config();
+const mongooseOption: mongoose.ConnectOptions = {
 
-mongoose.connect(process.env.MONGO_URI ?? '', { useNewUrlParser: true, useUnifiedTopology: true });
+};
+mongoose.connect(process.env.MONGO_URI ?? '', mongooseOption);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
